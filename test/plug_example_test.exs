bb5183d011e81c41cb8001c7ca4227ebd88893fc
Yan Mihailov <yanmhlv@gmail.com>
2017-12-11 23:56:00 +0300

    added tests on router

--- test/plug_example_test.exs
+++ test/plug_example_test.exs
@@ -1,8 +1,38 @@
 defmodule PlugExampleTest do
   use ExUnit.Case
-  doctest PlugExample
+  use Plug.Test
 
-  test "greets the world" do
-    assert PlugExample.hello() == :world
+  alias PlugExample.Router
+
+  @content "<html><body>Hi!</body></html>"
+  @mimetype "text/html"
+
+  @opts Router.init([])
+
+  test "returns welcome" do
+    conn =
+      conn(:get, "/", "")
+      |> Router.call(@opts)
+    assert conn.state == :sent
+    assert conn.status == 200
+  end
+
+  test "returns uploaded" do
+    conn =
+      conn(:post, "/upload", "content=#{@content}&mimetype=#{@mimetype}")
+      |> put_req_header("content-type", "application/x-www-form-urlencoded")
+      |> Router.call(@opts)
+
+    assert conn.state == :sent
+    assert conn.status == 201
+  end
+
+  test "returns 404" do
+    conn =
+      conn(:get, "/missing", "")
+      |> Router.call(@opts)
+
+    assert conn.state == :sent
+    assert conn.status == 404
   end
 end
